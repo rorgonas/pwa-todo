f6e83b90b32d805c76a57544e0dde3e031b27221
Rolland Orgonas <rorgonas@gmail.com>
2021-02-07 13:37:19 -0500

    pwa-todo - update - config

--- backend/config.js
+++ backend/config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  "type": "service_account",
-  "project_id": "pwa-todo-1-95249",
-  "private_key_id": "b58e787d0984de506e30c15459f2507bf97e8663",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDLBORs9bn7FMEt\nBEpUqJGhIIDXVmjQCfjGbBlBZ8s7C5J0/qqKi8mgV/3t2LlMtfyS3Xo34JtL7vig\nqwtdWyFcqC1TPoj0oENCLkN987skDb0ka+x8LRGveTfDrNv/Qwo9GaajdP5/tmUV\nwpALB6IshWe3ccKlwGNDfAHsmTAiCuwIZ1mwWdFN1HXSoCw6ET6B2L1Vi0T+dNW3\nTjmX87cI435kqL56PnRNfQzd/v1avf7NJ13RfPxg9MT9kLy4JER0qogyT6gcPCxN\nol3jdRM2zJ/Hg/DUm70Y/ncqQaC4XaSRID8QRQL89A8TZdW9wevE2IB3j6xaUJHK\nha0VGHxfAgMBAAECggEAH4ipgGEkm6LMJJVjhEXQpPHbAVebHSFHYm6c96NLFmdE\nPublj3KKBV1VhFGi6o7j5d3/41kAVimOtcoLkFsc0fsPWzw94p1YBRoRgqlOepml\ntNgMmfcYJaTS7BJX84yytO3ci8wa0bR/fANGA2iSoI6yHvTNEpjQ8kT7OYhLySWY\n/t6ao8fmh49jWg/cNa5HwgnybECIwb7mRvAdXx9QASsENDBlzQuoILvTAcDgmcWU\nBHi2MZIAWVy+BhvtM8+FtImkIGNi8UOpRblUecsYJwtoM7jtRTwELaKlg+Lki8Mm\n1OeYjOX+cpcIt07rkihs6KFjQsZp/CgDE2KekzwDNQKBgQD7QVlTzeNHAORHLd09\nkEKZrt6LF2P7CoLOeBEf8VzNqzOWjdqK22iOpUJWwX61x6mYnW8iBmvxJcApMJ3Q\n4wOUDI338gnJh/dvB/GFdtQb30uZ1BgKabmjKbJZrYluIWQkbRFl+V2CqHMBxGWG\nFM3uV3/sVDCEzjwh/qRqk7vlvQKBgQDO2lqTNcEPbPEdawcNjqwQ6tNa8NXR37wP\nEoO/UtpIDQXEH3hBsUh4PQjiz5y57xgGntup7KKRc+NbPD7GKqPJyuyZ22oJkR9R\nPJ9GK23nQXwjYk+Yc+3yz/Q6zApcF/Tr0CbNm/oXB+bom7Nyk6o3BBd7+pbAX5RZ\nYQgAjozGSwKBgBmMY5Dp9ppZtNyoBT8lVCBcb7SlBD2arAJioxskZ9Nh/TTp57cG\n6HLYyw4XTtMdIlmqaIqFLKSwugF2OBXtf4emXyk/K0+ay8Y2gJCZrKfI5N8XefpV\nFdNJgiEsTDUuoAg0Lvi3ipKmnksntUsVAnuh2hgl12kpr2OliQJtU0XhAoGAZeqL\nqfBYOONyQQ5UoUdkW8l3bV0v3IIhxENZClEmH03bQ3BF7ggNLsDTufyUub+l1M08\naZKf3o58vJ0ISNcFcA09WekTa7dG+N2nlAkd000efsiXcwPbf0EakkWp7QoYsH8U\nP0XUiLhp+r2MQq2itgycDYe/8vSkwQzfxnZ1wdUCgYAiODxIl7GJ54vng0kQplQc\nXiayPvvyn18qCWRm+2AZA8VbBGbELvWtd0vnyKFZ6YSGJICh1znCStLaggnmBCQM\npWlvq4VIJ/a4ZZ+7sVqhYSR6TTPlRlYz0mMjMmFe9RgL3SKZgUZbhJF5kUWlqOBY\ntp831dRUtTHcUAjdLxZovw==\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-foymz@pwa-todo-1-95249.iam.gserviceaccount.com",
-  "client_id": "106143837172796740013",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-foymz%40pwa-todo-1-95249.iam.gserviceaccount.com"
+  "type": process.env.FIREBASE_TYPE,
+  "project_id": process.env.FIREBASE_PROJECT_ID,
+  "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY,
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  "client_id": process.env.FIREBASE_CLIENT_ID,
+  "auth_uri": process.env.FIREBASE_AUTH_URI,
+  "token_uri": process.env.FIREBASE_TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  "client_x509_cert_url": process.env.FIREBASE_CLIENT_CERT_URL
 }
