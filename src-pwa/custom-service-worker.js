f35d31311dd2af193da2f76d69c31e4fbf672027
Rolland Orgonas <rorgonas@gmail.com>
2021-02-13 11:12:03 -0500

    pwa-todo - sw - enable precache

--- src-pwa/custom-service-worker.js
+++ src-pwa/custom-service-worker.js
@@ -3,3 +3,17 @@
  * is picked up by the build system ONLY if
  * quasar.conf > pwa > workboxPluginMode is set to "InjectManifest"
  */
+
+
+/*
+ * Dependencies
+ *  */
+
+import { precacheAndRoute } from 'workbox-precaching';
+
+/*
+* Config
+* */
+
+// Use with precache injection
+precacheAndRoute(self.__WB_MANIFEST);
